abcbf167e164aed41cfefd84f64a03f85358a5e6
Mann <chanmannlim@gmail.com>
2011-09-10 15:03:15 +0700

    test

--- app/models/mo_message_observer.rb
+++ app/models/mo_message_observer.rb
@@ -1,6 +1,6 @@
 class MoMessageObserver < ActiveModel::Observer
   def after_create(mo_message)
-    logger.info 'creating an mt_message'
+    logger.error 'creating an mt_message'
     MtMessage.create :to => mo_message.from, :body => "you send: #{mo_message.body}"
   end
 end
--- app/models/mt_message_observer.rb
+++ app/models/mt_message_observer.rb
@@ -1,6 +1,8 @@
 class MtMessageObserver < ActiveModel::Observer
   def after_create(mt_message)
+    logger.error 'sending mt_message...'
     Nuntium.send_ao mt_message.attributes.slice('to', 'body')
+    logger.error 'done'
   end
 end
 
