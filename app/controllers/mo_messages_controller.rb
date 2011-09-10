7b234e13e191ec2c1bfd577cdbc3a0958e027d3c
Mann <chanmannlim@gmail.com>
2011-09-10 11:17:40 +0700

    filter mo message params

--- app/controllers/mo_messages_controller.rb
+++ app/controllers/mo_messages_controller.rb
@@ -2,7 +2,7 @@ class MoMessagesController < ApplicationController
   http_basic_authenticate_with :name => "sms", :password => "dating"
 
   def create
-    MoMessage.create(params)
+    MoMessage.create(params.slice :from, :body, :guid)
   end
 end
 
