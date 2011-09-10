b55751006d8982977996b78349c6468849f68a5a
Mann <chanmannlim@gmail.com>
2011-09-10 14:42:15 +0700

    test observer

--- app/models/mo_message_observer.rb
+++ app/models/mo_message_observer.rb
@@ -1,6 +1,9 @@
 class MoMessageObserver < ActiveRecord::Observer
+  :observe mo_message
+
   def after_create(mo_message)
-    MtMessage.create :to => mo_message.from, :body => "you send: #{mo_message.body}"
+    m = MtMessage.create :to => mo_message.from, :body => "you send: #{mo_message.body}"
+    p m
   end
 end
 
