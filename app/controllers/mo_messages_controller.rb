e8ac184fcb1b8e80dc669f9923e0e2d892d4af20
Mann <chanmannlim@gmail.com>
2011-09-10 14:02:56 +0700

    move hardcoded nuntium endpoint authentication to config/nuntium.yml

--- app/controllers/mo_messages_controller.rb
+++ app/controllers/mo_messages_controller.rb
@@ -1,5 +1,5 @@
 class MoMessagesController < ApplicationController
-  http_basic_authenticate_with :name => "sms", :password => "dating"
+  http_basic_authenticate_with :name => Nuntium::CONFIG['incoming_user'], :password => Nuntium::CONFIG['incoming_password']
 
   def create
     MoMessage.create(params.slice :from, :body, :guid)
