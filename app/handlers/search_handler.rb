108c2aeb4511dc6d558a6b2d2b0c832c10fbf2b3
David Wilkie <dwilkie@gmail.com>
2011-10-07 19:04:01 +0700

    Added landing page and removed old rails 3 files

--- app/handlers/search_handler.rb
+++ app/handlers/search_handler.rb
@@ -34,12 +34,19 @@ class SearchHandler < MessageHandler
   end
 
   def extract_gender(body, force_update)
+    # test cases:
+
+    # want girl friend
+    # want girlfriend
+    # jong rok mit srey
+    # kjom broh jong rok mit srey
+
     # if there's 1 gender, then assume that's what the user is looking for
     # if there's 2 genders then the first one is their gender, 2nd is what their looking for
     # if they say *friend* without a gender then they r looking for a friend (don't know gender)
     # if they say gender + *friend* or *friend* + gender they're looking for that gender
     # keywords: girlfriend, boyfriend, friend, srey, broh, girl, boy, \bm\b, \bf\b, man, woman, bf, gf, bfriend, gfriend
-
+    match = strip_match!(body, /||/)
 
   end
 
