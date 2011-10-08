7e190cad9589365a3487e218b247c6c7f2d6e0b3
David Wilkie <dwilkie@gmail.com>
2011-10-08 22:05:28 +0700

    fix authentication for messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate
-
+  #http_basic_authenticate_with :name => ENV["CHAT_BOX_USERNAME"], :password => ENV["CHAT_BOX_PASSWORD"]
   # force_ssl # this breaks the tests...
 
   protected
 
   def authenticate
-    authenticate_with_http_basic do |name, password|
-      name == ENV["CHAT_BOX_USERNAME"] && password == ENV["CHAT_BOX_PASSWORD"]
-    end
+    request_http_basic_authentication unless authenticate_with_http_basic {|username, password| username == ENV["CHAT_BOX_USERNAME"] && password == ENV["CHAT_BOX_PASSWORD"]}
   end
 end
 
