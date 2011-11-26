b9d3d5e2df0bc31befcbb18b10f7df10dde74ad1
David Wilkie <dwilkie@gmail.com>
2011-11-26 17:55:47 +0700

    clean up messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,12 @@ class MessagesController < ApplicationController
     message = Message.new(params.slice :from, :body)
     from = message.origin
     message.user = User.find_or_initialize_by_mobile_number(from)
-    unless user.location_id?
-      location = user.message.build_location
-      location.country_code = Location.country_code(from)
-    end 
-    message.save!
-    message.process! # move this into a background process
-    render :nothing => true, :status => :created, :location => messages_path(message)
+    if message.save
+      message.process! # move this into a background process
+      response_params = {:status => :created, :location => messages_path(message)}
+    else
+      response_params = {:status => :bad_request}
+    end
+    render :nothing => true, response_params
   end
 end
-
