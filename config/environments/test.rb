199f02e0c224bf529d643430d681bb5074483626
David Wilkie <dwilkie@gmail.com>
2012-01-30 17:06:54 +0700

    Add defaults for Rails 3.2.0 application

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,8 +37,8 @@ Chibi::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
-  config.assets.allow_debugging = true
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 
   # Override global setting so that errors are raised when the translation file is wrong
   config.i18n.fallbacks = false
