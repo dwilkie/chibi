955d0609c49c18a0204f002c34ea4958bc6b4759
David Wilkie <dwilkie@gmail.com>
2012-02-20 22:25:38 +0700

    Print debug info for workers not notifying when chat has ended

--- app/workers/chat_deactivator.rb
+++ app/workers/chat_deactivator.rb
@@ -2,7 +2,20 @@ class ChatDeactivator
   @queue = :chat_deactivator_queue
 
   def self.perform(chat_id, options = {})
+    p "***********************"
+    p "start chat deactivator"
+    p ""
+    p "chat id:"
+    p chat_id
     chat = Chat.find(chat_id)
+    p "chat"
+    p chat
+    p "chat.active?"
+    p chat.active?
+    p "options"
+    p options
     chat.deactivate!(options)
+    p "end chat deactivator"
+    p "**********************"
   end
 end
