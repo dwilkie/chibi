6a30f3a924430695ffb4324efc0ef4b88b6b5a4c
David Wilkie <dwilkie@gmail.com>
2012-03-18 13:08:23 +0700

    added debug lines to fix precompilation error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
 if Rails.env.production?
+  p "redis to go url while precompiling is:"
+  p ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS
