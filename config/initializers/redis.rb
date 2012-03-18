983aa150a929a276db1ed90c3312642830332c4b
David Wilkie <dwilkie@gmail.com>
2012-03-18 13:10:42 +0700

    Fix heroku precompile error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,4 @@
-if Rails.env.production?
-  p "redis to go url while precompiling is:"
-  p ENV["REDISTOGO_URL"]
+if ENV["REDISTOGO_URL"] && Rails.env.production?
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS
