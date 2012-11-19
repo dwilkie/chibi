cc2ebfadd759f71964647530abb3eb6148918ab0
David Wilkie <dwilkie@gmail.com>
2012-11-19 18:26:43 +0700

    try touching the queue to see if it restarts

--- app/workers/message_processor.rb
+++ app/workers/message_processor.rb
@@ -2,6 +2,7 @@ class MessageProcessor
   @queue = :message_processor_queue
 
   def self.perform(message_id)
+    p "try touching the worker..."
     message = Message.find(message_id)
     message.process!
   end
