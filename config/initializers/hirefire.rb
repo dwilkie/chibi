e494dc2b5ce921ede3cad28a0210fed9c86c8690
David Wilkie <dwilkie@gmail.com>
2013-02-25 14:24:34 +0700

    main worker should only scale for processing messages and dialing

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,11 +1,15 @@
 HireFire::Resource.configure do |config|
   config.dyno(:worker) do
-    HireFire::Macro::Resque.queue
+    HireFire::Macro::Resque.queue(:message_processor_queue, :dialer_queue)
   end
 
   config.dyno(:highloadworker) do
     HireFire::Macro::Resque.queue(
-      :chat_reactivator_queue, :chat_deactivator_queue, :chat_expirer_queue
+      :chat_reactivator_queue,
+      :chat_deactivator_queue,
+      :chat_expirer_queue,
+      :reminderer_queue,
+      :friend_finder_queue
     )
   end
 end
