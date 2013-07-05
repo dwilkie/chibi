dd31db0d5bb69bd403ed3a6ac4b7e1be49dca417
David Wilkie <dwilkie@gmail.com>
2013-07-05 10:01:22 +0700

    add last_active_at to users

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620064836) do
+ActiveRecord::Schema.define(version: 20130705025726) do
 
-  create_table "call_data_records", :force => true do |t|
+  create_table "call_data_records", force: true do |t|
     t.text     "body"
     t.string   "uuid"
     t.integer  "duration"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(:version => 20130620064836) do
     t.datetime "rfc2822_date"
     t.string   "direction"
     t.integer  "phone_call_id"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "type"
     t.integer  "inbound_cdr_id"
     t.string   "bridge_uuid"
@@ -30,118 +30,119 @@ ActiveRecord::Schema.define(:version => 20130620064836) do
     t.integer  "user_id"
   end
 
-  add_index "call_data_records", ["bridge_uuid"], :name => "index_call_data_records_on_bridge_uuid"
-  add_index "call_data_records", ["direction"], :name => "index_call_data_records_on_direction"
-  add_index "call_data_records", ["from"], :name => "index_call_data_records_on_from"
-  add_index "call_data_records", ["inbound_cdr_id"], :name => "index_call_data_records_on_inbound_cdr_id"
-  add_index "call_data_records", ["phone_call_id", "type"], :name => "index_call_data_records_on_phone_call_id_and_type", :unique => true
-  add_index "call_data_records", ["user_id"], :name => "index_call_data_records_on_user_id"
-  add_index "call_data_records", ["uuid"], :name => "index_call_data_records_on_uuid", :unique => true
+  add_index "call_data_records", ["bridge_uuid"], name: "index_call_data_records_on_bridge_uuid", using: :btree
+  add_index "call_data_records", ["direction"], name: "index_call_data_records_on_direction", using: :btree
+  add_index "call_data_records", ["from"], name: "index_call_data_records_on_from", using: :btree
+  add_index "call_data_records", ["inbound_cdr_id"], name: "index_call_data_records_on_inbound_cdr_id", using: :btree
+  add_index "call_data_records", ["phone_call_id", "type"], name: "index_call_data_records_on_phone_call_id_and_type", unique: true, using: :btree
+  add_index "call_data_records", ["user_id"], name: "index_call_data_records_on_user_id", using: :btree
+  add_index "call_data_records", ["uuid"], name: "index_call_data_records_on_uuid", unique: true, using: :btree
 
-  create_table "chats", :force => true do |t|
+  create_table "chats", force: true do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "starter_id"
     t.string   "starter_type"
   end
 
-  add_index "chats", ["friend_id"], :name => "index_chats_on_friend_id"
-  add_index "chats", ["starter_type", "starter_id"], :name => "index_chats_on_starter_type_and_starter_id"
-  add_index "chats", ["updated_at"], :name => "index_chats_on_updated_at"
-  add_index "chats", ["user_id"], :name => "index_chats_on_user_id"
+  add_index "chats", ["friend_id"], name: "index_chats_on_friend_id", using: :btree
+  add_index "chats", ["starter_type", "starter_id"], name: "index_chats_on_starter_type_and_starter_id", using: :btree
+  add_index "chats", ["updated_at"], name: "index_chats_on_updated_at", using: :btree
+  add_index "chats", ["user_id"], name: "index_chats_on_user_id", using: :btree
 
-  create_table "locations", :force => true do |t|
+  create_table "locations", force: true do |t|
     t.string   "city"
-    t.string   "country_code", :limit => 2
+    t.string   "country_code", limit: 2
     t.float    "latitude"
     t.float    "longitude"
     t.integer  "user_id"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "locations", ["country_code"], :name => "index_locations_on_country_code"
-  add_index "locations", ["user_id"], :name => "index_locations_on_user_id"
+  add_index "locations", ["country_code"], name: "index_locations_on_country_code", using: :btree
+  add_index "locations", ["user_id"], name: "index_locations_on_user_id", using: :btree
 
-  create_table "messages", :force => true do |t|
+  create_table "messages", force: true do |t|
     t.string   "from"
     t.text     "body"
     t.integer  "user_id"
     t.integer  "chat_id"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "guid"
-    t.string   "state",      :default => "received", :null => false
+    t.string   "state",      default: "received", null: false
   end
 
-  add_index "messages", ["chat_id"], :name => "index_messages_on_chat_id"
-  add_index "messages", ["guid"], :name => "index_messages_on_guid", :unique => true
-  add_index "messages", ["state"], :name => "index_messages_on_state"
-  add_index "messages", ["user_id"], :name => "index_messages_on_user_id"
+  add_index "messages", ["chat_id"], name: "index_messages_on_chat_id", using: :btree
+  add_index "messages", ["guid"], name: "index_messages_on_guid", unique: true, using: :btree
+  add_index "messages", ["state"], name: "index_messages_on_state", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
-  create_table "missed_calls", :force => true do |t|
+  create_table "missed_calls", force: true do |t|
     t.string   "from"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "missed_calls", ["user_id"], :name => "index_missed_calls_on_user_id"
+  add_index "missed_calls", ["user_id"], name: "index_missed_calls_on_user_id", using: :btree
 
-  create_table "phone_calls", :force => true do |t|
+  create_table "phone_calls", force: true do |t|
     t.string   "sid"
     t.string   "from"
     t.string   "state"
     t.integer  "user_id"
     t.integer  "chat_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "dial_call_sid"
   end
 
-  add_index "phone_calls", ["chat_id"], :name => "index_phone_calls_on_chat_id"
-  add_index "phone_calls", ["dial_call_sid"], :name => "index_phone_calls_on_dial_call_sid", :unique => true
-  add_index "phone_calls", ["sid"], :name => "index_phone_calls_on_sid", :unique => true
-  add_index "phone_calls", ["state"], :name => "index_phone_calls_on_state"
-  add_index "phone_calls", ["user_id"], :name => "index_phone_calls_on_user_id"
+  add_index "phone_calls", ["chat_id"], name: "index_phone_calls_on_chat_id", using: :btree
+  add_index "phone_calls", ["dial_call_sid"], name: "index_phone_calls_on_dial_call_sid", unique: true, using: :btree
+  add_index "phone_calls", ["sid"], name: "index_phone_calls_on_sid", unique: true, using: :btree
+  add_index "phone_calls", ["state"], name: "index_phone_calls_on_state", using: :btree
+  add_index "phone_calls", ["user_id"], name: "index_phone_calls_on_user_id", using: :btree
 
-  create_table "replies", :force => true do |t|
+  create_table "replies", force: true do |t|
     t.string   "to"
     t.text     "body"
     t.integer  "user_id"
     t.integer  "chat_id"
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.datetime "delivered_at"
     t.string   "token"
-    t.string   "state",        :default => "pending_delivery", :null => false
+    t.string   "state",        default: "pending_delivery", null: false
   end
 
-  add_index "replies", ["chat_id"], :name => "index_replies_on_chat_id"
-  add_index "replies", ["state"], :name => "index_replies_on_state"
-  add_index "replies", ["token"], :name => "index_replies_on_token", :unique => true
-  add_index "replies", ["user_id"], :name => "index_replies_on_user_id"
+  add_index "replies", ["chat_id"], name: "index_replies_on_chat_id", using: :btree
+  add_index "replies", ["state"], name: "index_replies_on_state", using: :btree
+  add_index "replies", ["token"], name: "index_replies_on_token", unique: true, using: :btree
+  add_index "replies", ["user_id"], name: "index_replies_on_user_id", using: :btree
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "mobile_number"
     t.string   "name"
     t.string   "screen_name"
     t.date     "date_of_birth"
-    t.string   "gender",         :limit => 1
-    t.string   "looking_for",    :limit => 1
+    t.string   "gender",         limit: 1
+    t.string   "looking_for",    limit: 1
     t.integer  "active_chat_id"
-    t.datetime "created_at",                                        :null => false
-    t.datetime "updated_at",                                        :null => false
-    t.string   "state",                       :default => "online", :null => false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "state",                    default: "online", null: false
+    t.datetime "last_active_at"
   end
 
-  add_index "users", ["active_chat_id"], :name => "index_users_on_active_chat_id"
-  add_index "users", ["date_of_birth"], :name => "index_users_on_date_of_birth"
-  add_index "users", ["gender"], :name => "index_users_on_gender"
-  add_index "users", ["looking_for"], :name => "index_users_on_looking_for"
-  add_index "users", ["mobile_number"], :name => "index_users_on_mobile_number", :unique => true
-  add_index "users", ["state"], :name => "index_users_on_state"
+  add_index "users", ["active_chat_id"], name: "index_users_on_active_chat_id", using: :btree
+  add_index "users", ["date_of_birth"], name: "index_users_on_date_of_birth", using: :btree
+  add_index "users", ["gender"], name: "index_users_on_gender", using: :btree
+  add_index "users", ["looking_for"], name: "index_users_on_looking_for", using: :btree
+  add_index "users", ["mobile_number"], name: "index_users_on_mobile_number", unique: true, using: :btree
+  add_index "users", ["state"], name: "index_users_on_state", using: :btree
 
 end
