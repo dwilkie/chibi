5fb79e1e3c8da8a5784b6ebf8988718b9c095fb9
David Wilkie <dwilkie@gmail.com>
2013-07-16 15:11:04 +0700

    fix the delivery_receipt_creator_spec

--- spec/workers/delivery_receipt_creator_spec.rb
+++ spec/workers/delivery_receipt_creator_spec.rb
@@ -9,7 +9,7 @@ describe DeliveryReceiptCreator do
 
   describe ".perform(params)" do
     let(:reply) { mock_model(Reply) }
-    let(:find_stub) { Reply.stub(:find_by_token!) }
+    let(:find_stub) { Reply.stub(:find_by_token) }
     let(:params) { { :token => :token, :state => :state} }
 
     before do
@@ -18,7 +18,7 @@ describe DeliveryReceiptCreator do
     end
 
     it "should update the delivery state of the reply" do
-      Reply.should_receive(:find_by_token!).with(:token)
+      Reply.should_receive(:find_by_token).with(:token)
       reply.should_receive(:update_delivery_state) do |options|
         options[:state].should == :state
         options[:force].should be_true
