e14674f3da01acd59012653b1a860a1eba550aea
David Wilkie <dwilkie@gmail.com>
2013-07-17 23:10:41 +0700

    add user_importer worker

---
+++ spec/workers/user_importer_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe UserImporter do
+  context "@queue" do
+    it "should == :user_importer_queue" do
+      subject.class.instance_variable_get(:@queue).should == :user_importer_queue
+    end
+  end
+
+  describe ".perform(data)" do
+    let(:import_stub) { User.stub(:import!) }
+    let(:data) { "some data" }
+
+    before do
+      import_stub
+    end
+
+    it "should instruct the user to import the data" do
+      User.should_receive(:import!).with(data)
+      subject.class.perform(data)
+    end
+
+    it_should_behave_like "rescheduling SIGTERM exceptions" do
+      let(:args) { [data] }
+      let(:error_stub) { import_stub }
+    end
+  end
+end
