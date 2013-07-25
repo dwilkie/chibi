083d49738dff506c540400b3446b37c3dbb34561
David Wilkie <dwilkie@gmail.com>
2013-07-25 16:40:05 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130720000848) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "call_data_records", force: true do |t|
     t.text     "body"
     t.string   "uuid"
