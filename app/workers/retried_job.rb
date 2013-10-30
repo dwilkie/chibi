000ace11b8821d63283f49c00c4699ab68eeb44e
David Wilkie <dwilkie@gmail.com>
2013-10-30 12:07:28 +0700

    temporarily use my fork of Resque to supress TermException errors from ResqueWeb

--- app/workers/retried_job.rb
+++ app/workers/retried_job.rb
@@ -2,6 +2,7 @@ module RetriedJob
   def on_failure_retry(exception, *args)
     if exception.is_a?(Resque::TermException)
       Resque.enqueue(self, *args)
+      raise Resque::DontFail
     end
   end
 end
--- spec/support/resque_reschedule_examples.rb
+++ spec/support/resque_reschedule_examples.rb
@@ -2,14 +2,14 @@ shared_examples_for "rescheduling SIGTERM exceptions" do
   context "given a 'Resque::TermException' is raised" do
     it "should reschedule the job immediately" do
       Resque.should_receive(:enqueue).with(subject.class, *args)
-      subject.class.on_failure_retry(Resque::TermException.new("SIGTERM"), *args)
+      expect { subject.class.on_failure_retry(Resque::TermException.new("SIGTERM"), *args) }.to raise_error(Resque::DontFail)
     end
   end
 
   context "given some other error is raised" do
     it "should not reschedule the job" do
       Resque.should_not_receive(:enqueue)
-      subject.class.on_failure_retry(ArgumentError, *args)
+      subject.class.on_failure_retry(StandardError.new, *args)
     end
   end
 end
