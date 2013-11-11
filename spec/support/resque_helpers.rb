0e1f6ded24bfe59dcf9f53a2f98bc581b94f5e4b
David Wilkie <dwilkie@gmail.com>
2013-11-11 22:18:26 +0700

    fix resque helpers

--- spec/support/resque_helpers.rb
+++ spec/support/resque_helpers.rb
@@ -8,7 +8,7 @@ module ResqueHelpers
 
     unless options.delete(:queue_only)
       # don't perform jobs that are not intended to be performed by this app
-      queues = ResqueSpec.queues.reject { |k, v| k =~ /mt_message_queue$/ || EXTERNAL_QUEUES.include?(k.to_sym) }
+      queues = ResqueSpec.queues.reject { |k, v| k =~ /mt_message_queue$/ || EXTERNAL_QUEUES.include?(k.to_s) }
       queues.keys.each do |queue_name|
         ResqueSpec.perform_all(queue_name)
       end
