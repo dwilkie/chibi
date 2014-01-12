7ce149faf64c5d34647b73b006152d28dc342a85
David Wilkie <dwilkie@gmail.com>
2014-01-12 10:52:42 +0700

    reorganize Procfile to so queues don't get overloaded

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,31 +1,84 @@
 HireFire::Resource.configure do |config|
-  config.dyno(:urgent_task_worker) do
+  config.dyno(:urgent_task_worker_1) do
     HireFire::Macro::Resque.queue(
-      :charge_request_updater_queue,
-      :message_processor_queue,
-      :dialer_queue,
-      :call_data_record_creator_queue,
-      :twilio_cdr_fetcher_queue,
-      :delivery_receipt_creator_queue,
-      :report_generator_queue
+      :charge_request_updater_queue
     )
   end
 
-  config.dyno(:non_essential_task_worker) do
+  config.dyno(:urgent_task_worker_2) do
     HireFire::Macro::Resque.queue(
-      :locator_queue,
-      :chat_reactivator_queue,
-      :chat_reinvigorator_queue,
+      :call_data_record_creator_queue
+    )
+  end
+
+  config.dyno(:urgent_task_worker_3) do
+    HireFire::Macro::Resque.queue(
+      :message_processor_queue
+    )
+  end
+
+  config.dyno(:urgent_task_worker_4) do
+    HireFire::Macro::Resque.queue(
+      :twilio_cdr_fetcher_queue
+    )
+  end
+
+  config.dyno(:urgent_task_worker_5) do
+    HireFire::Macro::Resque.queue(
+      :delivery_receipt_creator_queue
+    )
+  end
+
+  config.dyno(:urgent_task_worker_6) do
+    HireFire::Macro::Resque.queue(
+      :dialer_queue
+    )
+  end
+
+  config.dyno(:non_essential_task_worker_1) do
+    HireFire::Macro::Resque.queue(
+      :user_creator_queue,
+      :user_importer_queue
+    )
+  end
+
+  config.dyno(:non_essential_task_worker_2) do
+    HireFire::Macro::Resque.queue(
       :chat_deactivator_queue,
-      :chat_expirer_queue,
+      :chat_expirer_queue
+    )
+  end
+
+  config.dyno(:non_essential_task_worker_3) do
+    HireFire::Macro::Resque.queue(
       :friend_messenger_queue,
-      :friend_finder_queue,
+      :friend_finder_queue
+    )
+  end
+
+  config.dyno(:non_essential_task_worker_4) do
+    HireFire::Macro::Resque.queue(
       :user_reminderer_queue,
-      :reminderer_queue,
-      :nuntium_ao_queryer_queue,
-      :blank_reply_fixer_queue,
-      :user_importer_queue,
-      :user_creator_queue
+      :reminderer_queue
+    )
+  end
+
+  config.dyno(:non_essential_task_worker_5) do
+    HireFire::Macro::Resque.queue(
+      :chat_reactivator_queue,
+      :chat_reinvigorator_queue
+    )
+  end
+
+  config.dyno(:non_essential_task_worker_6) do
+    HireFire::Macro::Resque.queue(
+      :locator_queue
+    )
+  end
+
+  config.dyno(:non_essential_task_worker_7) do
+    HireFire::Macro::Resque.queue(
+      :report_generator_queue
     )
   end
 end
