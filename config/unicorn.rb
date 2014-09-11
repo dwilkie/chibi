969fd6e9fafb8079e55946628e2dcf5241c20bfe
David Wilkie <dwilkie@gmail.com>
2014-09-11 15:33:49 +0700

    Revert "all env var for overriding request timeout"
    This reverts commit a35552fa453e41345f2b9d60fcd742647ea8765c.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # From: https://devcenter.heroku.com/articles/rails-unicorn
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout Integer(ENV["REQUEST_TIMEOUT"] || 15)
+timeout 15
 preload_app true
 
 before_fork do |server, worker|
