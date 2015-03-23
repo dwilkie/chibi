c226331655aee79668c3ed718e5f46d065e2bd28
David Wilkie <dwilkie@gmail.com>
2015-03-23 17:13:50 +0700

    Try to explicitly ask job to retry. #161

--- app/jobs/mt_message_update_status_job.rb
+++ app/jobs/mt_message_update_status_job.rb
@@ -1,6 +1,6 @@
 class MtMessageUpdateStatusJob
   include Sidekiq::Worker
-  sidekiq_options :queue => Rails.application.secrets[:smpp_mt_message_update_status_queue]
+  sidekiq_options(:retry => true, :dead => true, :queue => Rails.application.secrets[:smpp_mt_message_update_status_queue])
 
   def perform(smsc_name, mt_message_id, smsc_message_id, status)
     Reply.find(mt_message_id).delivered_by_smsc!(smsc_name, smsc_message_id, status)
