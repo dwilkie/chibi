196082ced551846c64ef64c1000fbde99bee8898
David Wilkie <dwilkie@gmail.com>
2015-06-19 13:24:07 +0700

    Remove secret token from source control. #183

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Chibi::Application.config.secret_key_base = '218f9a490e9249169003a3b9cd246ffadbe43510ac18a1b0e314b4cf6e780fc09c9f902cb2622a791b100d60882a58cd090abcaf8dee771162aca3a31c9cac88'
