b1bf5e4e80643a16db4a93bb61a363c28dea64b1
David Wilkie <dwilkie@gmail.com>
2015-07-11 02:21:57 +0700

    Optionally turn off AWS configuration. #183

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => Rails.application.secrets[:aws_access_key_id],
-    :aws_secret_access_key  => Rails.application.secrets[:aws_secret_access_key],
-  }
-  config.fog_directory  = Rails.application.secrets[:aws_fog_directory]
-  config.fog_public     = false
+if (Rails.application.secrets[:aws_configure] || 1).to_i == 1
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => Rails.application.secrets[:aws_access_key_id],
+      :aws_secret_access_key  => Rails.application.secrets[:aws_secret_access_key],
+    }
+    config.fog_directory  = Rails.application.secrets[:aws_fog_directory]
+    config.fog_public     = false
+  end
 end
